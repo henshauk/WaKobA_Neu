04a394613a978839804148b5adeb36072fb32769
HenningSGE <henning_luebbers@t-online.de>
2017-05-22 16:08:04 +0200

    Testklassen erweitert

--- src/whiteBoxTests/AuthentifiTest.java
+++ src/whiteBoxTests/AuthentifiTest.java
@@ -2,25 +2,151 @@ package whiteBoxTests;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import data.Authentifi;
 
 public class AuthentifiTest {
 	
-	@Test
+	//TBD: file festlegen und ggf. Logindaten eintragen, die schon vor dem Test existieren müssen
+
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+		System.setOut(null);
+	}
+
+	@Ignore
+	public void testSetFileNew() {
+		// erstellt eine neue Datei im angegeben Ordner
+		String dir = ""; // TBD
+		Authentifi.setFile(dir);
+		assertEquals("create login file" + System.getProperty("line.separator"), outContent.toString());
+	}
+
+	@Ignore
+	public void testSetFileExisting() {
+		// Datei existiert schon => Bestätigungsmeldung?
+		String dir = ""; // TBD
+		Authentifi.setFile(dir);
+	}
+
+	@Ignore
 	public void testValidGood() {
-		//testet den Login mit gültigen Daten
+		// testet den Login mit gültigen Daten
 		String user = "test";
 		String pass = "testpw";
 		boolean accessGranted = false;
-		try{
+		try {
 			accessGranted = Authentifi.valid(user, pass);
-		} catch(IOException e) {
-			System.out.println("IOException aufgetreten!");
+		} catch (IOException e) {
+			fail("IOException ist aufgetreten!");
 		}
 		assertTrue(accessGranted);
+	}
+
+	@Ignore
+	public void testValidBadUsername() {
+		// testet den Login mit nichtexistentem Usernamen und Passwort,
+		// das von jemand anderem benutzt wird
+		String user = "test";
+		String pass = "testpw";
+		boolean accessGranted = true;
+		try {
+			accessGranted = Authentifi.valid(user, pass);
+		} catch (IOException e) {
+			fail("IOException ist aufgetreten!");
+		}
+		assertFalse(accessGranted);
+	}
+
+	@Ignore
+	public void testValidBadPassword() {
+		// testet den Login mit gültigem Usernamen, aber falschem Passwort
+		String user = "test";
+		String pass = "testpw";
+		boolean accessGranted = true;
+		try {
+			accessGranted = Authentifi.valid(user, pass);
+		} catch (IOException e) {
+			fail("IOException ist aufgetreten!");
+		}
+		assertFalse(accessGranted);
+	}
+
+	@Ignore
+	public void testReadLogins() {
+		try {
+			Authentifi.readLogins();
+		} catch (IOException e) {
+			fail("IOException ist aufgetreten!");
+		}
+		// TBD: Arrays kontrollieren?
+	}
+
+	@Ignore
+	public void testWriteLogins() {
+		String[] users = { "user1", "user2" };
+		String[] passwords = { "pw1", "pw2" };
+		try {
+			Authentifi.writeLogins(users, passwords);
+		} catch (IOException e) {
+			fail("IOException ist aufgetreten!");
+		}
+		// TBD: kontrollieren, ob Daten in Datei
+		// => mit userAusgeben()? Erst separat testen.
+	}
+
+	@Ignore
+	public void testNewUserGood() {
+		// legt einen neuen Benutzer an, dessen Username noch nicht existiert
+		String user = "user";
+		String pw = "pw";
+		try {
+			assertEquals(Authentifi.newUser(user, pw),
+					"Benutzer " + user + " erfolreich angelegt!");
+		} catch (IOException e) {
+			fail("IOException ist aufgetreten");
+		}
+	}
+	
+	@Ignore
+	public void testNewUserBadUsername() {
+		//versucht einen neuen Nutzer anzulegen, dessen Username bereits vergeben ist
+		String user = "user";
+		String pw = "pw";
+		try {
+			assertEquals(Authentifi.newUser(user, pw),
+					"Benutzer " + user + " erfolreich angelegt!");
+		} catch (IOException e) {
+			fail("IOException ist aufgetreten");
+		}
+	}
+	
+	@Ignore
+	public void testNewUserMaxUsers() {
+		//versucht einen neuen Nutzer anzulegen, aber es gibt bereits eine maximale Anzahl an Nutzern
+		//Username ist noch nicht vergeben!
+		String user = "user";
+		String pw = "pw";
+		try {
+			assertEquals(Authentifi.newUser(user, pw),
+					"Benutzer " + user + " konnte nicht angelegt werden!");
+		} catch (IOException e) {
+			fail("IOException ist aufgetreten");
+		}
 	}
 
 }
--- src/whiteBoxTests/WekabuilderTest.java
+++ src/whiteBoxTests/WekabuilderTest.java
@@ -2,14 +2,55 @@ package whiteBoxTests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import data.Wekabuilder;
 
 public class WekabuilderTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructorGood() {
+		//testet den Konstruktor mit funktionierendem Pfad und Ordner
+		String path = ""; //richtiger Pfad
+		String dataDir = ""; //richtiger Ordner
+		Wekabuilder w;
+		try {
+			w = new Wekabuilder(path, dataDir);
+			System.out.println(w.data);
+			assertTrue(w.data != null);
+		}
+		catch(Exception e) {
+			fail("Exception ist aufgetreten!");
+		}
 	}
 
+	@Test
+	public void testConstructorBadPath() {
+		//testet den Konstruktor mit richtigem Ordner, aber falschem Pfad
+		String path = ""; //falscher Pfad
+		String dataDir = ""; //richtiger Ordner
+		Wekabuilder w;
+		try {
+			w = new Wekabuilder(path, dataDir);
+			assertTrue(w.data != null);
+		}
+		catch(Exception e) {
+			fail("Exception ist aufgetreten!");
+		}
+	}
+
+	@Test
+	public void testConstructorBadDataDir() {
+		//testet den Konstruktor mit funktionierendem Pfad, aber falschem Ordner
+		String path = ""; //richtiger Pfad
+		String dataDir = ""; //falscher Ordner
+		Wekabuilder w;
+		try {
+			w = new Wekabuilder(path, dataDir);
+			assertTrue(w.data != null);
+		}
+		catch(Exception e) {
+			fail("Exception ist aufgetreten!");
+		}
+	}
 }
